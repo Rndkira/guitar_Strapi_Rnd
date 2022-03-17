1db1e1012380e267bba3d054d374e9fc91af8612
randy <randycanela89@gmail.com>
2022-03-17 11:47:04 -0400

    deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '90fdd95520400fe232c9913c07ed1cfe'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd572cee8-02a3-4dd5-89c1-4397d51b8c49'
+};
